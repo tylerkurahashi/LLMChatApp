49fcf34e36c96ad8058b5b0b34872dfcdac33781
tylerkurahashi <tyler.kurahashi@gmail.com>
2024-03-02 17:04:30 +0900

    [ADD] using Agent

--- app.py
+++ app.py
@@ -2,37 +2,45 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage
 
 from const import OPENAI_API_MODEL, OPENAI_API_TEMPERATURE
 
 load_dotenv()
 
+
+def create_agent_chain():
+    chat = ChatOpenAI(
+        model=OPENAI_API_MODEL, temperature=OPENAI_API_TEMPERATURE, streaming=True
+    )
+
+    tools = load_tools(["ddg-search", "wikipedia"])
+    return initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS)
+
+
 st.title("llm-chat-app")
 
 if "messages" not in st.session_state:
-  st.session_state.messages = []
-  
+    st.session_state.messages = []
+
 for message in st.session_state.messages:
-  with st.chat_message(message["role"]):
-    st.markdown(message["content"])
-    
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 prompt = st.chat_input("What is up?")
 
 if prompt:
-  st.session_state.messages.append({"role": "user", "content": prompt})
-  
-  with st.chat_message("user"):
-    st.markdown(prompt)
-    
-  with st.chat_message("assistant"):
-    chat = ChatOpenAI(
-      model=OPENAI_API_MODEL,
-      temperature=OPENAI_API_TEMPERATURE
-    )
-    messages = [HumanMessage(content=prompt)]
-    response = chat(messages)
-    st.markdown(response.content)
-    
-  st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        callback = StreamlitCallbackHandler(st.container())
+        agent_chain = create_agent_chain()
+        response = agent_chain.run(prompt, callbacks=[callback])
+        st.markdown(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
